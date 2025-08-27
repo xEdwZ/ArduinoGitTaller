42834b2e2508f06f0e52c75159a8e656e30541bf
xEdwZ <edwinlopezgonzalez25@gmail.com>
2025-08-27 07:28:39 -0500

    Add files via upload

---
+++ sketch.ino
@@ -0,0 +1,41 @@
+void inicio(){
+  Serial.println("Iniciando lectura...");
+  delay(1000);
+}
+
+int LecturaSensor() {
+  digitalWrite(13, HIGH);
+  int lectura= analogRead(A0);
+  delay(2000);
+  digitalWrite(13, LOW);
+  return lectura;
+}
+
+float convertir(int voltaje){
+  float volt= voltaje*(5.0/1023.0);
+  return volt;
+}
+
+void imprimir(float vol){
+  Serial.print("El voltaje es de ");
+  Serial.println(vol);
+}
+
+
+
+void loop(){
+  inicio();
+  int leer= LecturaSensor();
+  Serial.print("Lectura ADC: ");
+  Serial.println(leer);
+  delay(1000);
+  
+  float volt=convertir(leer);
+  imprimir(volt);
+}
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
